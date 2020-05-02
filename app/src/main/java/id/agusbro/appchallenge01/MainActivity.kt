ceba0cbd55e9f1c2ba7e04d2783d25509ac956d7
agus_prayogi <agusyogi600@gmail.com>
2020-05-02 21:01:21 +0700

    layout_Home

--- app/src/main/java/id/agusbro/appchallenge01/MainActivity.kt
+++ app/src/main/java/id/agusbro/appchallenge01/MainActivity.kt
@@ -1,12 +1,24 @@
 package id.agusbro.appchallenge01
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSpinner()
+    }
+
+    private fun setSpinner() {
+        val adapter = ArrayAdapter.createFromResource(this,R.array.jenisK,android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        jenisKl.adapter = adapter
     }
 }
