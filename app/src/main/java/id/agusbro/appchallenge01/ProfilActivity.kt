7c7fd062b169694e39ac51a4eb70e4053ecd44e8
agus_prayogi <agusyogi600@gmail.com>
2020-05-03 17:44:57 +0700

    layout_profile

---
+++ app/src/main/java/id/agusbro/appchallenge01/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package id.agusbro.appchallenge01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
